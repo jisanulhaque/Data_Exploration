8965b0238aa52aa13be303513439653b4ed85711
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-25 23:47:47 +0600

    death percentage and effected percentage extracted from Covid Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,10 +1,23 @@
 /*
 Portfolio Project
 */
-SELECT *
-FROM PortfolioProjects..CovidDeaths
-Order By 3,4
 
-SELECT *
-FROM PortfolioProjects..CovidVaccinations
-Order By 3,4 DESC
+SELECT location, date, total_cases, new_cases, total_deaths, population
+FROM PortfolioProjects..CovidDeaths
+Order By 1, 2
+
+--Looking at total cases vs total deaths
+--Shows death percentage in my country
+--Here altered columns 'total_cases' and 'total_deaths' from nvarchar to float for calculation
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE location like 'B%ng%sh'
+Order By 2 DESC --for getting latest info
+
+--Looking for total cases vs population
+SELECT location, date, population, total_cases, (total_cases/population)*100 as AffectedPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE location like 'B%ng%sh'
+ORDER BY 1,2 DESC
+
+
